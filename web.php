2e9c1516e3d2bee0447ce6937f0465f9c7e1de23
maryam2abd <66317556+maryam2abd@users.noreply.github.com>
2020-06-22 20:47:13 +0300

    Add files via upload

--- web.php
+++ web.php
@@ -13,31 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-
-Route::get('/', 'CourseController@index')->name('main_index');
-
-Route::get('/course/{id}', 'CourseController@show')->name('show_course');
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-
-// Admin / create , store , update, delete
-// User  / index , show 
-
-
-// prefix /admin/create .. /admin/strore .. /admin/update ....
-
-Route::group(['prefix'=>'admin','middleware'=>'auth'],function () {
-    Route::get('/', 'HomeController@index')->name('admin_index');
-
-    Route::get('profile','ProfileController@index')->name('profile');
-    Route::put('profile-update','ProfileController@updateProfile')->name('profile.update');
-
-    Route::get('/create', 'CourseController@create')->name('course_create');
-    Route::post('/store', 'CourseController@store')->name('course_create');
-    Route::get('/edit/{id}', 'CourseController@edit')->name('course_edit');
-    Route::put('/update/{id}', 'CourseController@update')->name('course_create');
-    Route::put('/delete', 'CourseController@destroy')->name('course_create');
-    
+Route::get('/', function () {
+    return view('welcome');
 });
+
+Route::get('/', 'Seuprojectcontroller@index');
+Route::get('/register', 'Seuprojectcontroller@register');
+Route::post('/registered', 'Seuprojectcontroller@registered');
+
