55d6135a2b4206fce66e54784b9f916fd3328bdb
Maryam-seu <57384754+Maryam-seu@users.noreply.github.com>
2020-07-02 01:05:24 +0300

    Add files via upload

--- ProfileController.php
+++ ProfileController.php
@@ -36,7 +36,22 @@ class ProfileController extends Controller
             $user->save();
 
         $request->session()->flash('message','Profile Successfully Updated');
-        return redirect()->route('admin_index');
+        return view('admin.profile');
 
        }
+       public function update_avatar(Request $request){
+
+    	// Handle the admin upload of avatar
+    	if($request->hasFile('avatar')){
+    		$avatar = $request->file('avatar');
+    		$filename = time() . '.' . $avatar->getClientOriginalExtension();
+    		Image::make($avatar)->resize(300, 300)->save( public_path('/uploads/avatars/' . $filename ) );
+
+    		$user = Auth::user();
+    		$user->avatar = $filename;
+    		$user->save();
+    	}
+
+    	return view('admin.profile');
+}
 }
